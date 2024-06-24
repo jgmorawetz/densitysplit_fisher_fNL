7ee9ee87ee03413aead64f932e8d64497632845b
James Morawetz <77302761+jgmorawetz@users.noreply.github.com>
2024-06-24 15:19:47 -0400

    Only plot up to kmax=0.5 for fiducial power.

--- plot_fiducial_power_spectra.py
+++ plot_fiducial_power_spectra.py
@@ -28,46 +28,48 @@ if __name__ == '__main__':
     fiducial_data = np.load(fiducial_path, allow_pickle=True).item()
     fiducial_power = fiducial_data['z']
     k_avg = fiducial_data['k_avg']
+    k_cut = k_avg <= 0.5
+    k_avg = k_avg[k_cut]
     # Creates plot
     fig, ax = plt.subplots(2, 2, sharex=True, sharey=False, dpi=400, figsize=(8, 4.8))
-    fig.subplots_adjust(hspace=0.15, wspace=0.1, left=0.07, right=0.98, top=0.95, bottom=0.1)
+    fig.subplots_adjust(hspace=0.15, wspace=0.15, left=0.07, right=0.98, top=0.95, bottom=0.1)
     # Halo power spectra
     halo_monopole, halo_quadropole = fiducial_power['h-h(0)'], fiducial_power['h-h(2)']
     halo_monopole_mean, halo_monopole_std = np.mean(halo_monopole, axis=0), np.std(halo_monopole, axis=0)
     halo_quadropole_mean, halo_quadropole_std = np.mean(halo_quadropole, axis=0), np.std(halo_quadropole, axis=0)
-    ax[0][0].fill_between(x=k_avg, y1=k_avg*(halo_monopole_mean-halo_monopole_std),
-                          y2=k_avg*(halo_monopole_mean+halo_monopole_std), alpha=0.2, color='black')
-    ax[0][0].plot(k_avg, k_avg*halo_monopole_mean, '-', color='black', label='Halo')
-    ax[1][0].fill_between(x=k_avg, y1=k_avg*(halo_quadropole_mean-halo_quadropole_std),
-                          y2=k_avg*(halo_quadropole_mean+halo_quadropole_std), alpha=0.2, color='black')
-    ax[1][0].plot(k_avg, k_avg*halo_quadropole_mean, '-', color='black')
+  #  ax[0][0].fill_between(x=k_avg, y1=k_avg**2*(halo_monopole_mean-halo_monopole_std),
+  #                        y2=k_avg**2*(halo_monopole_mean+halo_monopole_std), alpha=0.2, color='black')
+  #  ax[0][0].plot(k_avg, k_avg**2*halo_monopole_mean, '-', color='black', label='Halo')
+  #  ax[1][0].fill_between(x=k_avg, y1=k_avg**2*(halo_quadropole_mean-halo_quadropole_std),
+  #                        y2=k_avg**2*(halo_quadropole_mean+halo_quadropole_std), alpha=0.2, color='black')
+  #  ax[1][0].plot(k_avg, k_avg**2*halo_quadropole_mean, '-', color='black')
     # Iterates through the quantile power spectra
     colors = ['mediumblue', 'cornflowerblue', 'grey', 'lightsalmon', 'firebrick'] # need to add/remove colors if using more/less than five quantiles
     for i in range(n_quantiles):
         color = colors[i]
-        quantile_auto_monopole, quantile_auto_quadropole = fiducial_power[f'{i+1}-{i+1}(0)'], fiducial_power[f'{i+1}-{i+1}(2)']
-        quantile_cross_monopole, quantile_cross_quadropole = fiducial_power[f'{i+1}-h(0)'], fiducial_power[f'{i+1}-h(2)']
+        quantile_auto_monopole, quantile_auto_quadropole = fiducial_power[f'{i+1}-{i+1}(0)'][:, k_cut], fiducial_power[f'{i+1}-{i+1}(2)'][:, k_cut]
+        quantile_cross_monopole, quantile_cross_quadropole = fiducial_power[f'{i+1}-h(0)'][:, k_cut], fiducial_power[f'{i+1}-h(2)'][:, k_cut]
         quantile_auto_monopole_mean, quantile_auto_monopole_std = np.mean(quantile_auto_monopole, axis=0), np.std(quantile_auto_monopole, axis=0)
         quantile_auto_quadropole_mean, quantile_auto_quadropole_std = np.mean(quantile_auto_quadropole, axis=0), np.std(quantile_auto_quadropole, axis=0)
         quantile_cross_monopole_mean, quantile_cross_monopole_std = np.mean(quantile_cross_monopole, axis=0), np.std(quantile_cross_monopole, axis=0)
         quantile_cross_quadropole_mean, quantile_cross_quadropole_std = np.mean(quantile_cross_quadropole, axis=0), np.std(quantile_cross_quadropole, axis=0)
-        ax[0][0].fill_between(x=k_avg, y1=k_avg*(quantile_auto_monopole_mean-quantile_auto_monopole_std),
-                              y2=k_avg*(quantile_auto_monopole_mean+quantile_auto_monopole_std), alpha=0.2, color=color)
-        ax[0][0].plot(k_avg, k_avg*quantile_auto_monopole_mean, '-', color=color, label=f'DS{i+1}')
-        ax[1][0].fill_between(x=k_avg, y1=k_avg*(quantile_auto_quadropole_mean-quantile_auto_quadropole_std),
-                              y2=k_avg*(quantile_auto_quadropole_mean+quantile_auto_quadropole_std), alpha=0.2, color=color)
-        ax[1][0].plot(k_avg, k_avg*quantile_auto_quadropole_mean, '-', color=color)
-        ax[0][1].fill_between(x=k_avg, y1=k_avg*(quantile_cross_monopole_mean-quantile_cross_monopole_std),
-                              y2=k_avg*(quantile_cross_monopole_mean+quantile_cross_monopole_std), alpha=0.2, color=color)
-        ax[0][1].plot(k_avg, k_avg*quantile_cross_monopole_mean, '-', color=color)
-        ax[1][1].fill_between(x=k_avg, y1=k_avg*(quantile_cross_quadropole_mean-quantile_cross_quadropole_std),
-                              y2=k_avg*(quantile_cross_quadropole_mean+quantile_cross_quadropole_std), alpha=0.2, color=color)
-        ax[1][1].plot(k_avg, k_avg*quantile_cross_quadropole_mean, '-', color=color)
+        ax[0][0].fill_between(x=k_avg, y1=k_avg**2*(quantile_auto_monopole_mean-quantile_auto_monopole_std),
+                              y2=k_avg**2*(quantile_auto_monopole_mean+quantile_auto_monopole_std), alpha=0.2, color=color)
+        ax[0][0].plot(k_avg, k_avg**2*quantile_auto_monopole_mean, '-', color=color, label=f'DS{i+1}')
+        ax[1][0].fill_between(x=k_avg, y1=k_avg**2*(quantile_auto_quadropole_mean-quantile_auto_quadropole_std),
+                              y2=k_avg**2*(quantile_auto_quadropole_mean+quantile_auto_quadropole_std), alpha=0.2, color=color)
+        ax[1][0].plot(k_avg, k_avg**2*quantile_auto_quadropole_mean, '-', color=color)
+        ax[0][1].fill_between(x=k_avg, y1=k_avg**2*(quantile_cross_monopole_mean-quantile_cross_monopole_std),
+                              y2=k_avg**2*(quantile_cross_monopole_mean+quantile_cross_monopole_std), alpha=0.2, color=color)
+        ax[0][1].plot(k_avg, k_avg**2*quantile_cross_monopole_mean, '-', color=color)
+        ax[1][1].fill_between(x=k_avg, y1=k_avg**2*(quantile_cross_quadropole_mean-quantile_cross_quadropole_std),
+                              y2=k_avg**2*(quantile_cross_quadropole_mean+quantile_cross_quadropole_std), alpha=0.2, color=color)
+        ax[1][1].plot(k_avg, k_avg**2*quantile_cross_quadropole_mean, '-', color=color)
     # Adjusts plot parameters
     ax[0][0].legend(); ax[1][0].set_xlabel(r'$k \ [hMpc^{-1}]$')
     ax[1][1].set_xlabel(r'$k \ [hMpc^{-1}]$')
-    ax[0][0].set_ylabel(r'$k P(k) \ [h^{-2}Mpc^2]$')
-    ax[1][0].set_ylabel(r'$k P(k) \ [h^{-2}Mpc^2]$')
+    ax[0][0].set_ylabel(r'$k^2 P(k) \ [h^{-1}Mpc]$')
+    ax[1][0].set_ylabel(r'$k^2 P(k) \ [h^{-1}Mpc]$')
     ax[0][0].set_title('Quantile Auto')
     ax[0][1].set_title('Quantile-Halo Cross')
     ax[0][0].set_xscale('log')
