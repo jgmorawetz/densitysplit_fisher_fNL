b5fab5bf907a4c1f720a68cf408b4f5a690f8b93
James Morawetz <77302761+jgmorawetz@users.noreply.github.com>
2024-08-22 12:46:24 -0400

    Adjusted plot formatting.

--- generate_plots/plot_contours.py
+++ generate_plots/plot_contours.py
@@ -8,14 +8,12 @@ if __name__ == '__main__':
       
     # Makes a triangle (contour) plot of the marginalized constraints fitted to the max wavenumber
     # for halo only, DS only or joint
-
-    # Specific hyperparameters
     query_type, n_randoms, n_quantiles, filter_radius = 'lattice', None, 5, 10
     ncov, nderiv = 15000, 500
     data_folder = f'/home/jgmorawe/projects/rrg-wperciva/jgmorawe/results/quijote/plot_results/fisher_convergence/power/{query_type}_{n_randoms}_{n_quantiles}_{filter_radius}'
     param_list = ['LC', 'EQ', 'OR_LSS', 'Mmin', 'h', 'ns', 'Om', 's8']
     param_means = [0, 0, 0, 3.2e13, 0.6711, 0.9624, 0.3175, 0.834]
-    param_list_labels = [r'$f_{NL}^{loc}$', r'$f_{NL}^{equil}$', r'$f_{NL}^{ortho}$', r'$M_{min}$', r'$h$', r'$n_s$', r'$\Omega_m$', r'$\sigma_8$']
+    param_list_labels = [r'$f_{\mathrm{NL}}^{\mathrm{loc}}$', r'$f_{\mathrm{NL}}^{\mathrm{equil}}$', r'$f_{\mathrm{NL}}^{\mathrm{ortho}}$', r'$M_{\mathrm{min}}$', r'$h$', r'$n_{\mathrm{s}}$', r'$\Omega_{\mathrm{m}}$', r'$\sigma_8$']
     combs = ['Halo', 'DS', 'Joint']
     comb_colors = ['tab:blue', 'tab:orange', 'tab:green']
     samples = []
@@ -27,13 +25,11 @@ if __name__ == '__main__':
             comb_label = 'DSC'
         elif comb == 'Joint':
             comb_label = 'Joint'
-        # parameter covariance matrix at the max wavenumber (final index of list of matrices)
+        # Parameter covariance matrix at the max wavenumber (final index of list of matrices)
         marginalized_data = np.load(os.path.join(data_folder, f'marginalized_{comb}_LC-EQ-OR_LSS-Mmin-h-ns-Om-s8_{ncov}_{nderiv}.npy'))[-1]
         samples_comb = np.random.multivariate_normal(mean=param_means, cov=marginalized_data, size=1000000)
         samples_comb = MCSamples(samples=samples_comb, names=param_list_labels, settings={'smooth_scale_2D':0.3, 'smooth_scale_1D':0.3})
         samples.append(samples_comb)
-
-    # Makes the triangle plot
     g = plots.get_subplot_plotter()
     g.settings.axes_labelsize = 25
     g.settings.figure_legend_loc = 'upper right'
@@ -47,4 +43,4 @@ if __name__ == '__main__':
     g.triangle_plot(roots=samples, 
                     legend_labels=['Halo', 'DSC', 'Joint'],
                     filled=True, contour_colors=comb_colors)
-    plt.savefig('/home/jgmorawe/projects/rrg-wperciva/jgmorawe/results/quijote/plot_results/constraints_contour_plot_including_all_auto.png', dpi=400)
+    plt.savefig('/home/jgmorawe/projects/rrg-wperciva/jgmorawe/results/quijote/plot_results/constraints_contour_plot_including_all_auto.pdf', dpi=500)
